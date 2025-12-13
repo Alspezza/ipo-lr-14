77004463d9634540529fb771b3241ed51ec033b1
Zahar <zpecian23@gmail.com>
2025-12-13 13:41:05 +0300

    1

---
+++ image_package/Handler/__init__.py
@@ -0,0 +1,50 @@
+from PIL import Image
+
+class ImageHandler():
+    def __init__(self, path):
+        self.path = path
+    
+    def image_load(self):
+        #Загрузка изображения
+        try:
+            image = Image.open(f"{self.path}")
+            self.image = image
+        except Exception as e:
+            raise TypeError("Неправильно задан путь")
+
+    def size_change(self):
+        try:
+            self.image = Image.open(f"{self.path}")
+            print(f" Изображение загружено: {self.image.size}")
+        except Exception as e:
+            raise TypeError(f"Ошибка загрузки: {e}")
+        #Изменение размера
+        try:
+            width = int(input("Введите новую ширину изображения (300): "))
+            heigth = int(input("Введите новую высоту изображения (300): "))
+            new_size = (width, heigth)
+            self.image = self.image.resize(new_size)
+        except ValueError:
+            raise ValueError("Некорректный ввод чисел для размера")
+        
+
+    def image_save_png(self):
+        #Сохранение изображения
+        if self.image is None:
+            raise ValueError("Сначала загрузите изображение (вызовите image_load())")
+        try:
+            agree = input("Вы уверены что хотите сохранить (перезаписать) изображение - (да\нет): ")
+            if agree == "да":
+                self.image.save(self.path, "PNG")
+            else:
+                print("Сохранение отенено")
+        except Exception as e:
+            raise TypeError(f"Ошибка: {e}")
+        
+
+    def connect_to_processor(self, processor):
+        #Отправка изображения в класс Processor
+        processor.set_image(self.image)
+        return processor
+
+
---
+++ image_package/Processor/__init__.py
@@ -0,0 +1,8 @@
+from ..Handler import ImageHandler
+from PIL import Image, ImageFilter
+
+class ImageProcessor:
+    def __init__(self, imagep = ImageHandler.self.image):
+        self.imagep = imagep
+    def
+    
